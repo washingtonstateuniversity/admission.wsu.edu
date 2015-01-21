1746c6ab2acde1e9c3547a017526c2369d8b576c
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-01-21 14:55:57 -0800

    Initialize child theme

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom functionality required by your child theme can go here. Use this
+ * to override any defaults provided by the Spine parent theme through
+ * the provided actions and filters.
+ */
+
+// Uncomment this line and replace the spine_child prefix with one of your own.
+//add_action( 'wp_enqueue_scripts', 'spine_child_enqueue_scripts', 11 );
+/**
+ * Hooking into wp_enqueue_scripts allows you to add custom Javascript to every front
+ * end page view. Using available APIs in WordPress, you can restrict this to
+ * limited page views as well.
+ *
+ * As with the action above, change the prefix of this function from spine_child
+ */
