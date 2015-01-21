f0915834a7b22f40d8afcfd8b4bf370140ee651d
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-01-21 15:44:06 -0800

    Retrieve site nav from primary site

---
+++ spine/site-navigation.php
@@ -0,0 +1,28 @@
+<?php
+	
+	if ( defined( 'WSU_LOCAL_CONFIG') && 'WSU_LOCAL_CONFIG' ) {
+		global $blog_id; switch_to_blog( 16 );
+	} else {
+		global $blog_id; switch_to_blog( 267 );
+	}
+
+?>
+
+<nav id="spine-sitenav" class="spine-sitenav">
+	<?php
+	$spine_site_args = array(
+		'theme_location'  => 'site',
+		'menu'            => 'site',
+		'container'       => false,
+		'container_class' => false,
+		'container_id'    => false,
+		'menu_class'      => null,
+		'menu_id'         => null,
+		'items_wrap'      => '<ul>%3$s</ul>',
+		'depth'           => 5,
+	);
+	wp_nav_menu( $spine_site_args );
+	?>
+</nav>
+
+<?php restore_current_blog(); ?>
