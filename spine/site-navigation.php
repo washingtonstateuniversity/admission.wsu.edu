323fef81e3092bbc1917744a2847b695c2e628e1
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-06-18 16:04:30 -0700

    Restore network-temp to blog menus

--- spine/site-navigation.php
+++ spine/site-navigation.php
@@ -1,24 +1,29 @@
 <?php
 
-$menu = "site";
 
-if ( get_current_blog_id() == 15 || get_current_blog_id() == 339 || get_current_blog_id() == 340 || get_current_blog_id() == 646  ) {
-
-	if ( defined( 'WSU_LOCAL_CONFIG') && WSU_LOCAL_CONFIG ) {
-		switch_to_blog( 16 );
-		$menu = "network-temp";
-	} else {
-		switch_to_blog( 267 );
-		$menu = "network-temp";
-	}
-	
-}
 
 ?>
 
 <nav id="spine-sitenav" class="spine-sitenav">
 	<?php
-	if ( function_exists( 'bu_navigation_display_primary' ) ) {
+		
+	$menu = "site";
+
+	if ( get_current_blog_id() == 15 || get_current_blog_id() == 339 || get_current_blog_id() == 340 || get_current_blog_id() == 646  ) {
+	
+		if ( defined( 'WSU_LOCAL_CONFIG') && WSU_LOCAL_CONFIG ) {
+			switch_to_blog( 16 );
+			$menu = "network-temp";
+		} else {
+			switch_to_blog( 267 );
+			$menu = "network-temp";
+		}
+		
+		$menu_source = "network";
+		
+	}	
+	
+	if ( function_exists( 'bu_navigation_display_primary' ) && $menu_source !== "menu" ) {
 		$bu_nav_args = array(
 			'post_types'      => array( 'page' ), // post types to display
 			'include_links'   => true, // whether or not to include BU Navigation links with pages
@@ -34,10 +39,11 @@ if ( get_current_blog_id() == 15 || get_current_blog_id() == 339 || get_current_
 			'echo'            => 1 // whether to display immediately or return
 		);
 		bu_navigation_display_primary( $bu_nav_args );
+		
 	} else {
 		$spine_site_args = array(
 			'theme_location'  => 'site',
-			'menu'            => 'site',
+			'menu'            => $menu,
 			'container'       => false,
 			'container_class' => false,
 			'container_id'    => false,
