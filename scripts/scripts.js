e461feeabec0fb9f154ca024ca028ccea3c2891d
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-06-18 15:40:41 -0700

    Automatically process section backgrounds

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,18 @@ function inspire() {
 
 (function($){
 	
+	var process_section_backgrounds = function() {
+		var $bg_sections = $('.section-wrapper-has-background');
+	
+		$bg_sections.each( function() {
+			var background_image = $(this).data('background');
+			$(this).css('background-image', 'url(' + background_image + ')' );
+		});
+	};
+	
 	$(document).ready( function() {
+		
+		process_section_backgrounds();
 
 		$("#info dt").on("click", function() {
 			$(this).parents("#info").toggleClass("opened");
