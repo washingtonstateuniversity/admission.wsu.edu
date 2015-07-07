500f5d6f4052e6d150311578da562995cb4f95e5
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-07-07 16:40:20 -0700

    Begin to add actions widget globally through footer

---
+++ parts/footers.php
@@ -0,0 +1,20 @@
+<footer class="main-footer">
+	<dl id="actions" class="action-items">
+		<dl id="info" class="action-item">
+			<dt><button>info</button></dt>
+			<dd><?php echo do_shortcode('[html_snippet id=129]') ?></dd>
+		</dl>
+		<dl id="visit" class="action-item">
+			<dt><button>visit</buton></dt>
+			<dd><?php echo do_shortcode('[html_snippet id=128]') ?></dd>
+		</dl>
+		<dl id="apply" class="action-item">
+			<dt><button>apply</buton></dt>
+			<dd><?php echo do_shortcode('[html_snippet id=266]') ?></dd>
+		</dl>
+		<dl id="chat" class="action-item">
+			<dt><button>chat</buton></dt>
+			<dd></dd>
+		</dl>
+	</dl>
+</footer>
