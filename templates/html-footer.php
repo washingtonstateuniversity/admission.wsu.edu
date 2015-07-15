0f6e4883a13e00ea8ece28dbc74ffa3b617fee3a
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-07-15 12:29:54 -0700

    Allow for wordpress sections to start app header

--- templates/html-footer.php
+++ templates/html-footer.php
@@ -1,3 +1,8 @@
 <?php /* Template Name: HTML Footer */ ?>
 
+	<?php get_template_part( 'parts/footers' ); ?>
+
+</main>
+
 <?php get_footer(); ?>
+
--- templates/html-header.php
+++ templates/html-header.php
@@ -1,3 +1,28 @@
 <?php /* Template Name: HTML Header */ ?>
 
 <?php get_header(); ?>
+
+<main class="spine-page-html">
+
+<?php get_template_part('parts/headers'); ?>
+<?php get_template_part('parts/featured-images'); ?>
+
+<section class="row side-right gutter pad-ends">
+
+	<div class="column one">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part('articles/article'); ?>
+
+		<?php endwhile; ?>
+
+	</div><!--/column-->
+
+	<div class="column two">
+
+		<?php get_sidebar(); ?>
+
+	</div><!--/column two-->
+
+</section>
