494db8f60a1b4fa51bbaf368fd5f288b8f64cf73
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-07-22 14:10:30 -0700

    Update retrieval of action bar content

--- parts/footers.php
+++ parts/footers.php
@@ -9,9 +9,9 @@ if ( defined( 'WSU_LOCAL_CONFIG') && WSU_LOCAL_CONFIG ) {
 	
 } else {
 	
-	$info_snippet = "703";
-	$apply_snippet = "705";
-	$visit_snippet = "704";
+	$info_snippet = "267-703";
+	$apply_snippet = "267-705";
+	$visit_snippet = "267-704";
 	$chat_snippet = "267-706";
 
 } ?>
@@ -20,18 +20,18 @@ if ( defined( 'WSU_LOCAL_CONFIG') && WSU_LOCAL_CONFIG ) {
 	<dl id="actions" class="action-items">
 		<dl id="info" class="action-item">
 			<dt><button>info</button></dt>
-			<dd><?php echo do_shortcode('[html_snippet id='.$info_snippet.']') ?>
+			<dd><?php echo get_template_part('parts/info'); ?>
 				<?php //$slices = json_decode(file_get_contents('https://goto.wsu.edu/info/LookupHighSchool?term=el+camino+fundamental'),true);
 					//print_r($slices); ?>
 			</dd>
 		</dl>
 		<dl id="visit" class="action-item">
 			<dt><button>visit</buton></dt>
-			<dd><?php echo do_shortcode('[html_snippet id='.$visit_snippet.']') ?></dd>
+			<dd><?php echo do_shortcode('[html_snippet id="'.$visit_snippet.'"]') ?></dd>
 		</dl>
 		<dl id="apply" class="action-item">
 			<dt><button>apply</buton></dt>
-			<dd><?php echo do_shortcode('[html_snippet id='.$apply_snippet.']') ?></dd>
+			<dd><?php echo do_shortcode('[html_snippet id="'.$apply_snippet.'"]') ?></dd>
 		</dl>
 		<dl id="chat" class="action-item">
 			<dt><button>chat</buton></dt>
