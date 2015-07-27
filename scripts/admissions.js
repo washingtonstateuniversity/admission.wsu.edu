02b9998844009b7184577f89e97cd5cd51b9ea3b
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-07-27 10:00:43 -0700

    Note inspiring lead

--- scripts/admissions.js
+++ scripts/admissions.js
@@ -50,7 +50,7 @@ function inspire() {
 		
 		if ( $(".row:first-of-type").hasClass("inspiring") ) {
 			
-			//$("body").addClass("inspiring-lead");
+			$("body").addClass("inspiring-lead");
 		
 		}
 		
