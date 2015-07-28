8770c4135641fefcd8b939e2cdd014cd262af9dd
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-07-28 16:24:28 -0700

    Strip web pre and post templates

--- web-template-pre.php
+++ web-template-pre.php
@@ -1,11 +1,10 @@
 
-<main class="spine-page-default">
+<main class="spine-app-template">
 	
 	<?php get_template_part('parts/headers'); ?>
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<section class="row single gutter marginalize-ends">
-			<?php the_content(); ?>
-	</section>
+	
+	<?php the_content(); ?>
+	
 	<?php endwhile; endif; ?>
-	<section class="row single gutter marginalize-ends">
