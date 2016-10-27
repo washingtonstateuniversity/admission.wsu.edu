aa4e6a74690cebcdfc20a29a524275fee0c28c08
Phil Cable <pcable@wsu.edu>
2016-10-27 16:52:21 -0700

    Update `single.php` with featured image position code from the Spine theme (#56)

--- single.php
+++ single.php
@@ -24,8 +24,13 @@ if ( function_exists( 'wsuwp_uc_get_object_type_slugs' ) && in_array( get_post_t
 } else {
 	if ( spine_has_featured_image() ) {
 		$featured_image_src = spine_get_featured_image_src();
+		$featured_image_position = get_post_meta( get_the_ID(), '_featured_image_position', true );
+
+		if ( ! $featured_image_position || sanitize_html_class( $featured_image_position ) !== $featured_image_position ) {
+			$featured_image_position = '';
+		}
 		?>
-		<figure class="featured-image" style="background-image: url('<?php echo $featured_image_src ?>');">
+		<figure class="featured-image <?php echo sanitize_html_class( $featured_image_position ); ?>" style="background-image: url('<?php echo $featured_image_src ?>');">
 			<?php spine_the_featured_image(); ?>
 		</figure>
 		<?php
