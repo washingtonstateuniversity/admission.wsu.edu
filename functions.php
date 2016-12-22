6658b0007b69e1da0931095f263b0de3af94d73c
Jeremy Felt <jeremy.felt@gmail.com>
2016-12-21 16:30:29 -0800

    Add the retargeting pixels used by Carnegie Communications

--- functions.php
+++ functions.php
@@ -23,8 +23,7 @@ class WSU_Admission_Theme {
 		add_filter( 'single_template', array( $this, 'single_template' ) );
 		add_filter( 'bu_navigation_filter_item_attrs', array( $this, 'bu_navigation_filter_item_atts' ), 10, 2 );
 		add_filter( 'wp_kses_allowed_html', array( $this, 'allow_download_attribute' ), 10 );
-		add_action( 'wp_head', array( $this, 'custom_tag_manager', 101 ) );
-		add_action( 'wp_footer', array( $this, 'custom_noscript_tag_manager', 11 ) );
+		add_action( 'wp_footer', array( $this, 'carnegie_tracking_tags', 101 ) );
 	}
 
 	/**
@@ -160,26 +159,43 @@ class WSU_Admission_Theme {
 	}
 
 	/**
-	 * Insert the Google Tag Manager container provided by Carnegie Communications
+	 * Insert tracking tags used for retargeting with Carnegie Communications
 	 */
-	public function custom_tag_manager() {
+	public function carnegie_tracking_tags() {
 		?>
-		<!-- Google Tag Manager via Carnegie Communications -->
-		<script>
-			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M9M4JKC');
+		<!-- Google Code for Remarketing Tag -->
+		<!--------------------------------------------------
+		Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
+		--------------------------------------------------->
+		<script type="text/javascript">
+			/* <![CDATA[ */
+			var google_conversion_id = 864713563;
+			var google_custom_params = window.google_tag_params;
+			var google_remarketing_only = true;
+			/* ]]> */
 		</script>
-		<!-- End Google Tag Manager -->
-		<?php
-	}
-
-	/**
-	 * Insert the noscript version of Google Tag Manager provided by Carnegie Communications
-	 */
-	public function custom_noscript_tag_manager() {
-		?>
-		<!-- Google Tag Manager via Carnegie Communications (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9M4JKC" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<!-- End Google Tag Manager (noscript) -->
+		<script type="text/javascript" src="https://www.googleadservices.com/pagead/conversion.js">
+		</script>
+		<noscript>
+			<div style="display:inline;">
+				<img height="1" width="1" style="border-style:none;" alt="" src="https://googleads.g.doubleclick.net/pagead/viewthroughconversion/864713563/?guid=ON&amp;script=0"/>
+			</div>
+		</noscript>
+		<!-- Facebook Pixel Code -->
+		<script>
+			!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+				n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+				n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+				t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+				document,'script','https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '1283395071698859'); // Insert your pixel ID here.
+			fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		               src="https://www.facebook.com/tr?id=1283395071698859&ev=PageView&noscript=1"
+			/></noscript>
+		<!-- DO NOT MODIFY -->
+		<!-- End Facebook Pixel Code -->
 		<?php
 	}
 }
